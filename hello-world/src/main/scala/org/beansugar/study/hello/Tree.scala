96e8b9801537cbaecb17d42a3c242a198b67b0f6
cee <archmagece@gmail.com>
2017-01-25 17:42:49 +0900

    1이 아니고 l이야

--- hello-world/src/main/scala/org/beansugar/study/hello/Tree.scala
+++ hello-world/src/main/scala/org/beansugar/study/hello/Tree.scala
@@ -5,7 +5,7 @@ package org.beansugar.study.hello
   * @since 2017-01-25 15
   */
 abstract class Tree {
-	case class Sum(1: Tree, r: Tree) extends Tree
+	case class Sum(l: Tree, r: Tree) extends Tree
 	case class Var(n: String) extends Tree
 	case class Const(v: Int) extends Tree
 }
