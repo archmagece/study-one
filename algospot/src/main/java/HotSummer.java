52510486ee7053203b3fc12d2b6360cf005f9fd1
archmagece <archmagece@gmail.com>
2017-02-16 00:57:20 +0900

    120ms input output 조절해주니 속도개선

--- algospot/src/main/java/HotSummer.java
+++ algospot/src/main/java/HotSummer.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.Scanner;
 
 /**
@@ -5,24 +6,66 @@ import java.util.Scanner;
  * @since 2017-02-16 00
  */
 public class HotSummer {
-	public static void main(String[] args){
-		Scanner scanner = new Scanner(System.in);
+	//120ms
+	public static void main(String[] args) throws IOException {
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int n = scanner.nextInt();
-		scanner.nextLine();
+		int n = Integer.parseInt(input.readLine().trim());
 
 		for(int i=0;i<n;i++){
-			int goal = scanner.nextInt();
-			scanner.nextLine();
+			int goal = Integer.parseInt(input.readLine().trim());
 			int aggregate = 0;
-			for(String usage : scanner.nextLine().split("\\s")){
+			for(String usage : input.readLine().split("\\s")){
 				aggregate += Integer.parseInt(usage);
 			}
 			if(goal>=aggregate){
-				System.out.println("YES");
+				writer.write("YES");
 			}else{
-				System.out.println("NO");
+				writer.write("NO");
 			}
+			writer.newLine();
 		}
+		writer.flush();
 	}
+//	128ms
+//	public static void main(String[] args) throws IOException {
+//		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+//
+//		int n = Integer.parseInt(input.readLine().trim());
+//
+//		for(int i=0;i<n;i++){
+//			int goal = Integer.parseInt(input.readLine().trim());
+//			int aggregate = 0;
+//			for(String usage : input.readLine().split("\\s")){
+//				aggregate += Integer.parseInt(usage);
+//			}
+//			if(goal>=aggregate){
+//				System.out.println("YES");
+//			}else{
+//				System.out.println("NO");
+//			}
+//		}
+//	}
+//	168ms
+//	public static void main(String[] args){
+//		Scanner scanner = new Scanner(System.in);
+//
+//		int n = scanner.nextInt();
+//		scanner.nextLine();
+//
+//		for(int i=0;i<n;i++){
+//			int goal = scanner.nextInt();
+//			scanner.nextLine();
+//			int aggregate = 0;
+//			for(String usage : scanner.nextLine().split("\\s")){
+//				aggregate += Integer.parseInt(usage);
+//			}
+//			if(goal>=aggregate){
+//				System.out.println("YES");
+//			}else{
+//				System.out.println("NO");
+//			}
+//		}
+//	}
 }
